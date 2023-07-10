138cff6bf03b0e40295ea3bcbd671e22e77f9c05
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-07-10 09:44:02 +0000

    {deviseno}

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,15 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'sessions/new'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/completion'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/confirm'
-  end
-  namespace :public do
-    get 'sessions/new'
-  end
-  namespace :public do
-    get 'registrations/new'  #device関係の指定があるため調べなおす
-  end
-  namespace :public do
-    get 'items/index' => 'public/items'
-    get 'items/show' => 'public/items/:id'
-  end
-  namespace :public do
-    get 'homes/top' => 'public/homes/'
-    get 'homes/about'
-  end
-  devise_for :customers
-  devise_for :admins
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
