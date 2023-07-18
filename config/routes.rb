9e1531b987fc52bd0bb24801c259830370efdc87
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-07-18 15:30:31 +0000

    {ルーティング修正完了}7-11

--- config/routes.rb
+++ config/routes.rb
@@ -15,31 +15,40 @@ root to: 'public/homes#top'
   namespace :public do
     
     get 'orders/new'
+    post 'orders/confirm' => '/orders/confirm'
     get 'orders/completion'
-    get 'orders/index' => 'orders'
-    get 'orders/show' => 'orders/:id'
-    get 'cart_items/index' => 'cart_items'
-    get 'customers/show' => 'customers/mypage'
+    post 'orders/create' => '/orders'
+    get 'orders/index' => '/orders'
+    get 'orders/show' => '/orders/:id'
+    get 'cart_items/index' => '/cart_items'
+    patch 'cart_items/update' => '/cart_items/:id'
+    delete 'cart_items/destroy' => '/cart_items/:id'
+    delete 'cart_items/destroy_all' => '/cart_items/destroy_all'
+    post 'cart_items/create' => '/cart_items'
+    get 'customers/show' => '/customers/mypage'
     get 'customers/edit'
+    patch 'customers/update' => '/customers'
     get 'customers/confirm'
-    get 'items/index' => 'public/items'
-    get 'items/show' => 'public/items/:id'
-    get 'homes/top' => 'public/homes/'
-    get 'homes/about'
+    patch 'customers/withdrawal' => '/customers/withdrawal'
+    get 'items/index' => '/items'
+    get 'items/show' => '/items/:id'
+    get 'homes/top' => '/'
+    get 'homes/about' => '/about'
   end
  
 
   namespace :admin do
    
     
-    get 'orders/show' => 'orders/:id'
-    get 'customers/index' => 'customers'
-    get 'customers/show' => 'customers/:id'
-    get 'customers/edit' => 'customers/:id:edit'
-    get 'items/index' => 'items'
-    get 'items/new' => 'items/new'
-    get 'items/show' => 'items/:id'
-    get 'items/edit' => 'items/:id/edit'
+    get 'orders/show' => '/admin/orders/:id'
+    get 'customers/index' => '/admin/customers'
+    get 'customers/show' => '/admin/customers/:id'
+    get 'customers/edit' => '/admin/customers/:id:edit'
+    get 'items/index' => '/admin/items'
+    get 'items/new' => '/admin/items/new'
+    get 'items/show' => '/admin/items/:id'
+    get 'items/edit' => '/admin/items/:id/edit'
+    get 'homes/top' => '/admin'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
