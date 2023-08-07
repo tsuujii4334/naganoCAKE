cef8e506743e84b5a1a3e3144a6a4b9fc8445262
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-08-07 13:36:02 +0000

    7-11{退会機能修正中}

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,18 +5,21 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
   
   def update
   end
 
   def confirm
+    @customer = Customer.find_by(customer: params[:customer])
   end
   
   def withdrawal
-    @customer = Customer.find(params[:id])
+    @customer = Customer.find_by(customer: params[:customer])
     @customer.update(is_daketed: true)
     flash[:notice] = "退会処理を実行いたしました。"
+    reset_session
     redirect_to root_path
   end
 end
