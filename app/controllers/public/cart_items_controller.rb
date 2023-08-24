10337fc85a0f1e501a498e22ec32bc6deceb3bcf
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-08-24 16:44:36 +0000

    {支払い機能制作途中}7-11

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,26 +9,26 @@ class Public::CartItemsController < ApplicationController
     cart_item = CartItem.new(cart_item_params)
     cart_item.customer_id = current_customer.id
     cart_item.save
-    redirect_to cart_items_path(cart_item.id)
+    redirect_to cart_items_path(params[:id])
   end
 
 
   def update
     cart_item = CartItem.find(params[:id])
     cart_item.update(cart_item_params)
-    redirect_to cart_items_path(cart_item.id)
+    redirect_to cart_items_path((params[:id]))
   end
 
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
-    redirect_to cart_items_path
+    redirect_to cart_items_path((params[:id]))
   end
 
   def destroy_all
     cart_items = current_customer.cart_items
     cart_items.destroy_all
-    redirect_to cart_items_path
+    redirect_to cart_items_path((params[:id]))
   end
 
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   belongs_to :customer
   
-  enum payment:{
+  enum payment_method:{
     credit_card: 0, #クレジットカード
     transfer: 1, #銀行振込
   }
