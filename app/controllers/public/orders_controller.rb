1da9464581372e53170f20643cd1df9b471b5561
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-08-27 16:16:32 +0000

    7-11{注文情報確認画面修正途中}

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,24 +1,32 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
-  
+
   def confirm
-    @order = Order.new(order_params)
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.first_name + current_customer.last_name
+    @orders = Order.new(order_params)
+    @orders.postal_code = current_customer.postal_code
+    @orders.address = current_customer.address
+    @orders.name = current_customer.first_name + current_customer.last_name
+    @total = 0
+    @postage = 800
   end
 
   def completion
   end
-  
+
   def create
-    
+
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def order_params
+      params.require(:order).permit(:payment_method, :address)
   end
 end
