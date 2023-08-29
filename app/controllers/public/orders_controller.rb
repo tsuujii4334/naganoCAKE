88b1b4bdc3fd0b7526195102371f0c1bc8fb5ec4
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-08-29 12:39:36 +0000

    {注文情報確認画面まで}7-11

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,11 +4,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_items = current_customer.cart_items
     @order = Order.new(order_params)
     @order.postal_code = current_customer.postal_code
     @order.address = current_customer.address
     @total = 0
-    @postage = 800
+    @postage = 800 #postage→送料
   end
 
   def completion
@@ -25,6 +26,10 @@ class Public::OrdersController < ApplicationController
   end
 
 private
+  def order_detail_parans
+
+  end
+
   def order_params
       params.require(:order).permit(:payment_method, :address)
   end
