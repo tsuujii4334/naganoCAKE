bf29c875a2545830daa63cfb8eb5ee4f94009bca
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-09-04 09:41:53 +0000

    {カート内の同一商品の追加処理実装途中}7-11

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,7 +6,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    cart_item = CartItem.new(cart_item_params)
+    @cart_item ||= current_customer.cart_items.build(item_id: params[:item_id])
+    @cart_item.amount = @cart_item.amount + params[:amount].to_i
+   # cart_item = CartItem.new(cart_item_params)
     cart_item.customer_id = current_customer.id
     cart_item.save
     redirect_to cart_items_path
